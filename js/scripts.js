bfd72ee98595df89fb3d01817b1471ecdefe934d
robashley <robashley.hello@gmail.com>
2016-08-01 14:15:46 +0100

    Changed carousel interval and added timed overlay for mobile
    Changed interval for mobile carousel to 2200. Added rule for overlay to fade in and fade out with a delay of 1000 when the screen width is less than 800px. Added rule which makes the overlay the same height as the image so that covers the whole image (Line 11).

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
   if(window.innerWidth <= 800 && window.innerHeight <= 600) {
         $('#myCarousel').carousel({
-            interval: 500
+            interval: 2200
         })
+
   }
+	
+  if ( $(window).width() <=800) {      
+		$('#overlay').fadeIn('fast').delay(1000).fadeOut('fast')
+		$("#overlay").css("height", $(".item img").height());
+		} 
 });
+
